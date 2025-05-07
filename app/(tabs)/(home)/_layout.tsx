72ac4bfe7809f30f9ffbc8010b757e892943d7f0
onamonapia <mirrundell@gmail.com>
2025-05-06 20:31:19 -0700

    form validation works

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -1,6 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
@@ -14,6 +14,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const router = useRouter();
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
   });
@@ -35,8 +36,8 @@ export default function RootLayout() {
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-      <Fab>
-        <FabLabel>Add</FabLabel>
+      <Fab onPress={() => router.navigate('/add-animal')}>
+        <FabLabel>Add</FabLabel>     
       </Fab>
     </ThemeProvider>
     
---
+++ app/(tabs)/(home)/add-animal.tsx
@@ -0,0 +1,62 @@
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Formik } from 'formik';
+import React from 'react';
+import { Button, Text, TextInput } from 'react-native';
+import { object, string } from 'yup';
+
+let animalSchema = object({
+    name: string().required("Name is required."),
+    animal: string().required("Animal is required."),
+});
+
+export default function AddAnimal() {
+    return (
+        
+        <ThemedView>
+            <ThemedText>Add Animal</ThemedText>
+            <Formik
+                initialValues = {{
+                    name: '',
+                    animal: '',
+                }}
+                validationSchema = {animalSchema}
+                // onSubmit = {(values) => console.log(values)}
+                onSubmit = {(values) => {
+                    console.log(values);
+                }}
+            >
+
+                {({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting}) => (
+                    <ThemedView>
+                        <Text>Name</Text>
+                        <TextInput 
+                            id="name"
+                            placeholder="Enter name here" 
+                            value={values.name} 
+                            onChangeText={handleChange('name')}
+                            onBlur={handleBlur}>                                
+                        </TextInput>
+                        {errors.name && touched.name && <Text>{errors.name}</Text>}
+                        <Text>Animal</Text>
+                        <TextInput 
+                            id="animal"
+                            placeholder="Enter animal type here" 
+                            value={values.animal} 
+                            onChangeText={handleChange('animal')}
+                            onBlur={handleBlur}>                                
+                        </TextInput>
+                        {errors.animal && touched.animal && <Text>{errors.animal}</Text>}
+                        <Button
+                            title="Submit"
+                            onPress={() => handleSubmit()}
+                        />
+                    </ThemedView>
+                )
+
+                }
+            
+            </Formik>
+        </ThemedView>
+    )
+}
