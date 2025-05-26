eb8b195668ccdb853c0d9d25efd03860dab37888
onamonapia <mirrundell@gmail.com>
2025-05-25 22:23:11 -0700

    please commit please

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,9 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
-  eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*"],
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    plugins: {
-      prettier: require("eslint-plugin-prettier"),
-    },
-    rules: {
-      "prettier/prettier": "error",
-      "import/no-unresolved": "off",
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
-      "comma-dangle": ["error", "always-multiline"],
-    },
-  },
-  {
-    // Override for test files
-    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-    plugins: {
-      "testing-library": require("eslint-plugin-testing-library"),
-    },
-    rules: {},
   },
 ]);
