6afe4ac985a6d689bc7279d41bc60678bd95d514
onamonapia <mirrundell@gmail.com>
2025-05-25 19:57:04 -0700

    i can commit again

--- components/__tests__/home-test.tsx
+++ components/__tests__/home-test.tsx
@@ -1,12 +1,11 @@
 import { expect, test } from "@jest/globals";
 // import { screen } from "@testing-library/react-native";
 
-// describe("HomeScreen", () => {
-//   test("Lists stuff", () => {
-//     expect(screen.getByRole("Text")).toBeTruthy();
-//   });
-// });
-
-test("it won't let me commi9t unrelated files without this", () => {
+test("it won't let me commit unrelated files without this", () => {
   expect(1 + 2).toEqual(3);
+});
+describe("HomeScreen", () => {
+  test("Lists stuff", () => {
+    expect(screen.getByRole("Text")).toBeTruthy();
+  });
 });
