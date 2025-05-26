dd49e0d1019d20112a95806206de9c5110bd0a36
onamonapia <mirrundell@gmail.com>
2025-05-25 19:48:52 -0700

    please let me commit already

---
+++ components/__tests__/home-test.tsx
@@ -0,0 +1,12 @@
+import { expect, test } from "@jest/globals";
+// import { screen } from "@testing-library/react-native";
+
+// describe("HomeScreen", () => {
+//   test("Lists stuff", () => {
+//     expect(screen.getByRole("Text")).toBeTruthy();
+//   });
+// });
+
+test("it won't let me commi9t unrelated files without this", () => {
+  expect(1 + 2).toEqual(3);
+});
